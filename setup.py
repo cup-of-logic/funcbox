ce322706222dc0b7786cf0fe162abba9e22816f9
Priyanshu Maity <priyanshu.maity2006@gmail.com>
2024-06-23 15:49:17 +0530

    Created setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='funcbox',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+
+    # Metadata about your project
+    author='Priyanshu Maity',
+    author_email='priyanshu.maity2006@gmail.com',
+    description='A collection of helper functions to improve code productivity',
+    url='https://github.com/cup-of-logic/funcbox',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
