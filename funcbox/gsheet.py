fe0a82b0e9f5032550e5127102506be17254b5f2
Priyanshu Maity <priyanshu.maity2006@gmail.com>
2024-07-24 19:47:47 +0530

    Changes made to structure

--- funcbox/gsheet.py
+++ funcbox/gsheet.py
@@ -34,33 +34,6 @@ def get_worksheet_data_as_list(worksheet_data):
             f"Datatype '{type(worksheet_data)}' for data is not supported. Supported datatypes: {SHEET_RETURN_TYPES}")
 
 
-def get_worksheet_as(worksheet, return_type: str = 'list_of_lists'):
-    if return_type == 'list_of_lists':
-        return worksheet.get_all_values()
-    elif return_type == 'list_of_dicts':
-        return worksheet.get_all_records()
-    elif return_type == 'array':
-        return np.array(worksheet.get_all_values())
-    elif return_type == 'dataframe':
-        sheet_list = get_worksheet_as(worksheet)
-        return pd.DataFrame(sheet_list[1:], columns=sheet_list[0])
-    else:
-        raise ValueError(f"Return type '{return_type}' is not supported. Supported return types: {SHEET_RETURN_TYPES}")
-
-
-def append_rows(worksheet, row_data):
-    row_data = get_worksheet_data_as_list(row_data)
-    n_rows = worksheet.row_count
-    worksheet.update(f'A{n_rows + 1}', row_data)
-
-
-def append_cols(worksheet, col_data, header: bool = True):
-    col_data = get_worksheet_data_as_list(col_data)
-    col_data = [[row[i] for row in col_data] for i in range(len(col_data[0]))]
-    n_cols = worksheet.col_count
-    worksheet.update(f'{get_column_name(n_cols + 1)}{2 if header else 1}', col_data)
-
-
 class Workbook:
     def __init__(self, creds_filepath: str, workbook_id: str):
         self.workbook_id = workbook_id
@@ -71,24 +44,79 @@ class Workbook:
 
         self.client = gspread.authorize(self.creds)
         self.workbook = self.client.open_by_key(self.workbook_id)
+        self.worksheets = self.workbook.worksheets()
+        self.worksheet = self.worksheets[0]
+        self.__truncate_empty_worksheets()
 
-    def __getitem__(self, key):
-        if isinstance(key, int):
-            return self.workbook.get_worksheet(key)
-        elif isinstance(key, str):
-            return self.workbook.worksheet(key)
+    def create_worksheet(self, name: str, headers: list, data=None, select: bool = True):
+        if data:
+            data = get_worksheet_data_as_list(data)
+        self.workbook.add_worksheet(name, rows=1 + (len(data) if data else 0), cols=len(headers))
+        worksheet = self.workbook.worksheet(name)
+        self.worksheets.append(worksheet)
+        worksheet.update(range_name="A1", values=[headers] + (data if data else []))
+
+        if select:
+            self.worksheet = worksheet
+
+    def remove_worksheet(self, select=0):
+        self.workbook.del_worksheet(self.worksheet)
+        self.worksheet = self.select_worksheet(select)
+
+    def select_worksheet(self, identifier):
+        if isinstance(identifier, int):
+            self.worksheet = self.workbook.get_worksheet(identifier)
+        elif isinstance(identifier, str):
+            self.worksheet = self.workbook.worksheet(identifier)
         else:
-            raise ValueError(f"Datatype for key in __getitem__ cannot be '{type(key)}'. Supported datatypes: (str, int)")
+            raise ValueError(f"Datatype for key in __getitem__ cannot be '{type(identifier)}'. Supported datatypes: (str, int)")
 
-    def __setitem__(self, key, data):
-        worksheet = self.__getitem__(key)
+    def get_worksheet_data(self, return_type: str = 'list_of_lists'):
+        if return_type == 'list_of_lists':
+            return self.worksheet.get_all_values()
+        elif return_type == 'list_of_dicts':
+            return self.worksheet.get_all_records()
+        elif return_type == 'array':
+            return np.array(self.worksheet.get_all_values())
+        elif return_type == 'dataframe':
+            sheet_list = self.get_worksheet_data(self.worksheet)
+            return pd.DataFrame(sheet_list[1:], columns=sheet_list[0])
+        else:
+            raise ValueError(
+                f"Return type '{return_type}' is not supported. Supported return types: {SHEET_RETURN_TYPES}")
+
+    def set_worksheet(self, data):
         data = get_worksheet_data_as_list(data)
-
         n_rows, n_cols = len(data), len(data[0])
-        worksheet.clear()
-        worksheet.update(f"A1:{chr(ord('A') + n_cols - 1)}{n_rows}", data)
 
+        self.worksheet.resize(rows=n_rows, cols=n_cols)
+        self.worksheet.update("A1", data)
 
-if __name__ == '__main__':
-    print(get_column_name(703))
+    def append_rows(self, row_data):
+        row_data = get_worksheet_data_as_list(row_data)
+        n_rows = self.worksheet.row_count
+        n_cols = self.worksheet.col_count
+        self.worksheet.resize(rows=n_rows + len(row_data), cols=n_cols)
+        self.worksheet.update(f'A{n_rows + 1}', row_data)
 
+    def append_cols(self, col_data, header: bool = True):
+        col_data = get_worksheet_data_as_list(col_data)
+        col_data = [[row[i] for row in col_data] for i in range(len(col_data[0]))]
+        n_rows = self.worksheet.row_count
+        n_cols = self.worksheet.col_count
+        self.worksheet.resize(rows=n_rows, cols=n_cols + len(col_data))
+        self.worksheet.update(f'{get_column_name(n_cols + 1)}{2 if header else 1}', col_data)
+
+    def __truncate_empty_worksheets(self):
+        for worksheet in self.worksheets:
+            if worksheet.acell('A1').value is None:
+                worksheet.resize(rows=1, cols=1)
+
+    def __str__(self):
+        return f'<Workbook "{self.workbook.title}" total_worksheets={len(self.worksheets)}>'
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __getattr__(self, item):
+        return getattr(self.workbook, item)
